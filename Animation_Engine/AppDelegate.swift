4d51e61bdd6e014a88bf67567de788893bba3076
Rick Twohy <twohy@cpe-2606-a000-1215-c9d2-0-0-0-ac8.dyn6.twc.com>
2018-10-07 19:25:30 -0400

    Initial Commit

---
+++ Animation_Engine/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Animation_Engine
+//
+//  Created by Rick Twohy on 10/7/18.
+//  Copyright © 2018 Rick Twohy. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Animation_Engine/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Animation_Engine
+//
+//  Created by Rick Twohy on 10/7/18.
+//  Copyright © 2018 Rick Twohy. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
